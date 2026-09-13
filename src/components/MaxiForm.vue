890b1c7d4b65533d1b030358089975e427ffc53e
Caroline Nordbrandt <caroline_nordbradt@hotmail.com>
2026-09-13 19:31:34 +0200

    feat: update header level in TheWelcome component for consistency
    add yatzy-tables

--- src/components/MaxiForm.vue
+++ src/components/MaxiForm.vue
@@ -1,8 +1,256 @@
-<script setup>
-</script>
+<script setup></script>
 
 <template>
-    <div>
-        <p>This will display a Maxi Yatzy game.</p>
-    </div>
+  <table class="yatzy-block">
+    <thead>
+      <tr class="summa-rad">
+        <th>SPELARE:</th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th scope="row">Ettor</th>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+      </tr>
+      <tr>
+        <th scope="row">Tvåor</th>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+      </tr>
+      <tr>
+        <th scope="row">Treor</th>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+      </tr>
+      <tr>
+        <th scope="row">Fyror</th>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+      </tr>
+      <tr>
+        <th scope="row">Femmor</th>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+      </tr>
+      <tr>
+        <th scope="row">Sexor</th>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+      </tr>
+
+      <tr class="summa-rad">
+        <th scope="row">SUMMA:</th>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+
+      <tr>
+        <th scope="row">BONUS</th>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+
+      <tr>
+        <th scope="row">Ett Par</th>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+      </tr>
+      <tr>
+        <th scope="row">Två Par</th>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+      </tr>
+      <tr>
+        <th scope="row">Tre Par</th>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+      </tr>
+      <tr>
+        <th scope="row">Tretal</th>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+      </tr>
+      <tr>
+        <th scope="row">Fyrtal</th>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+      </tr>
+      <tr>
+        <th scope="row">Femtal</th>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+      </tr>
+      <tr>
+        <th scope="row">Liten stege</th>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+      </tr>
+      <tr>
+        <th scope="row">Stor stege</th>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+      </tr>
+      <tr>
+        <th scope="row">Full stege</th>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+      </tr>
+      <tr>
+        <th scope="row">Kåk</th>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+      </tr>
+      <tr>
+        <th scope="row">Hus</th>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+      </tr>
+      <tr>
+        <th scope="row">Torn</th>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+      </tr>
+      <tr>
+        <th scope="row">Chans</th>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+      </tr>
+      <tr>
+        <th scope="row">YATZY</th>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+        <td><input type="number" /></td>
+      </tr>
+
+      <tr class="summa-rad">
+        <th scope="row">SUMMA:</th>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+    </tbody>
+  </table>
 </template>
+
+<style scoped>
+.yatzy-block {
+  border-collapse: collapse;
+  width: 100%;
+}
+
+th,
+td {
+  border: 2px solid #1a1a1a;
+  padding: 4px 8px;
+  text-align: left;
+}
+
+.summa-rad {
+  background-color: #c9b8a3;
+  font-weight: bold;
+}
+
+input {
+  width: 100%;
+  border: none;
+  background: transparent;
+  text-align: center;
+}
+</style>
--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -5,7 +5,7 @@ const openReadmeInEditor = () => fetch('/__open-in-editor?file=README.md')
 
 <template>
   <div>
-    <h1>Welcome to Emo Yatzy!</h1>
+    <h2>Welcome to Emo Yatzy!</h2>
     <p>This is a simple Yatzy game built with Vue.js.</p>
     <p>Choose a game mode from the navigation above to get started.</p>
   </div>
