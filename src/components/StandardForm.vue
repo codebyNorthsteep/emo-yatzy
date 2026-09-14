6cdf9fe252c58631c7a0a87528ad976ff5eebb82
Caroline Nordbrandt <caroline_nordbradt@hotmail.com>
2026-09-14 11:06:05 +0200

    add yatzy-logig to StandardForm component

--- src/components/StandardForm.vue
+++ src/components/StandardForm.vue
@@ -1,187 +1,188 @@
-<script setup></script>
+<script setup>
+import { ref, reactive } from "vue";
+const playerName = ref("");
+const players = ref([]);
+
+const points = reactive([]);
+
+const kategorier = ["ettor", "tvaor", "treor", "fyror", "femmor", "sexor"];
+const kombinationer = [
+  "ettPar",
+  "tvaPar",
+  "tretal",
+  "fyrtal",
+  "litenStege",
+  "storStege",
+  "kak",
+  "chans",
+  "yatzy",
+];
+
+function addPlayer() {
+  const id = Date.now();
+  players.value.push({ id, name: playerName.value });
+  points[id] = {};
+  playerName.value = "";
+}
+
+function topSum(playerId) {
+  return kategorier.reduce(
+    (sum, k) => sum + (Number(points[playerId]?.[k]) || 0),
+    0,
+  );
+}
+
+function bonus(playerId) {
+  return topSum(playerId) >= 63 ? 50 : 0;
+}
+
+function lowerSum(playerId) {
+  return kombinationer.reduce(
+    (sum, k) => sum + (Number(points[playerId]?.[k]) || 0),
+    0,
+  );
+}
+
+function totalSum(playerId) {
+  return topSum(playerId) + bonus(playerId) + lowerSum(playerId);
+}
+
+function terminateGame() {
+  players.value = [];
+  Object.keys(points).forEach((key) => delete points[key]);
+}
+</script>
 
 <template>
+  <form @submit.prevent="addPlayer">
+    <input class="player-input" type="text" v-model="playerName" />
+    <button type="button">Add Player</button>
+  </form>
   <table class="yatzy-block">
     <thead>
       <tr class="summa-rad">
         <th>SPELARE:</th>
-        <th></th>
-        <th></th>
-        <th></th>
-        <th></th>
-        <th></th>
-        <th></th>
+        <th v-for="player in players" :key="player.id">
+          {{ player.name }}
+        </th>
       </tr>
     </thead>
     <tbody>
       <tr>
         <th scope="row">Ettor</th>
-        <td><input type="number" /></td>
-        <td><input type="number" /></td>
-        <td><input type="number" /></td>
-        <td><input type="number" /></td>
-        <td><input type="number" /></td>
-        <td><input type="number" /></td>
+        <td v-for="player in players" :key="player.id">
+          <input type="number" v-model="points[player.id].ettor" />
+        </td>
       </tr>
       <tr>
         <th scope="row">Tvåor</th>
-        <td><input type="number" /></td>
-        <td><input type="number" /></td>
-        <td><input type="number" /></td>
-        <td><input type="number" /></td>
-        <td><input type="number" /></td>
-        <td><input type="number" /></td>
+        <td v-for="player in players" :key="player.id">
+          <input type="number" v-model="points[player.id].tvaor" />
+        </td>
       </tr>
       <tr>
         <th scope="row">Treor</th>
-        <td><input type="number" /></td>
-        <td><input type="number" /></td>
-        <td><input type="number" /></td>
-        <td><input type="number" /></td>
-        <td><input type="number" /></td>
-        <td><input type="number" /></td>
+        <td v-for="player in players" :key="player.id">
+          <input type="number" v-model="points[player.id].treor" />
+        </td>
       </tr>
       <tr>
         <th scope="row">Fyror</th>
-        <td><input type="number" /></td>
-        <td><input type="number" /></td>
-        <td><input type="number" /></td>
-        <td><input type="number" /></td>
-        <td><input type="number" /></td>
-        <td><input type="number" /></td>
+        <td v-for="player in players" :key="player.id">
+          <input type="number" v-model="points[player.id].fyror" />
+        </td>
       </tr>
       <tr>
         <th scope="row">Femmor</th>
-        <td><input type="number" /></td>
-        <td><input type="number" /></td>
-        <td><input type="number" /></td>
-        <td><input type="number" /></td>
-        <td><input type="number" /></td>
-        <td><input type="number" /></td>
+        <td v-for="player in players" :key="player.id">
+          <input type="number" v-model="points[player.id].femmor" />
+        </td>
       </tr>
       <tr>
         <th scope="row">Sexor</th>
-        <td><input type="number" /></td>
-        <td><input type="number" /></td>
-        <td><input type="number" /></td>
-        <td><input type="number" /></td>
-        <td><input type="number" /></td>
-        <td><input type="number" /></td>
+        <td v-for="player in players" :key="player.id">
+          <input type="number" v-model="points[player.id].sexor" />
+        </td>
       </tr>
 
       <tr class="summa-rad">
         <th scope="row">SUMMA:</th>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
+        <td v-for="player in players" :key="player.id">
+          {{ topSum(player.id) }}
+        </td>
       </tr>
 
       <tr>
         <th scope="row">BONUS</th>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
+        <td v-for="player in players" :key="player.id">
+          {{ bonus(player.id) }}
+        </td>
       </tr>
 
       <tr>
         <th scope="row">Ett Par</th>
-        <td><input type="number" /></td>
-        <td><input type="number" /></td>
-        <td><input type="number" /></td>
-        <td><input type="number" /></td>
-        <td><input type="number" /></td>
-        <td><input type="number" /></td>
+        <td v-for="player in players" :key="player.id">
+          <input type="number" v-model="points[player.id].ettPar" />
+        </td>
       </tr>
       <tr>
         <th scope="row">Två Par</th>
-        <td><input type="number" /></td>
-        <td><input type="number" /></td>
-        <td><input type="number" /></td>
-        <td><input type="number" /></td>
-        <td><input type="number" /></td>
-        <td><input type="number" /></td>
+        <td v-for="player in players" :key="player.id">
+          <input type="number" v-model="points[player.id].tvaPar" />
+        </td>
       </tr>
       <tr>
         <th scope="row">Tretal</th>
-        <td><input type="number" /></td>
-        <td><input type="number" /></td>
-        <td><input type="number" /></td>
-        <td><input type="number" /></td>
-        <td><input type="number" /></td>
-        <td><input type="number" /></td>
+        <td v-for="player in players" :key="player.id">
+          <input type="number" v-model="points[player.id].tretal" />
+        </td>
       </tr>
       <tr>
         <th scope="row">Fyrtal</th>
-        <td><input type="number" /></td>
-        <td><input type="number" /></td>
-        <td><input type="number" /></td>
-        <td><input type="number" /></td>
-        <td><input type="number" /></td>
-        <td><input type="number" /></td>
+        <td v-for="player in players" :key="player.id">
+          <input type="number" v-model="points[player.id].fyrtal" />
+        </td>
       </tr>
       <tr>
         <th scope="row">Liten stege</th>
-        <td><input type="number" /></td>
-        <td><input type="number" /></td>
-        <td><input type="number" /></td>
-        <td><input type="number" /></td>
-        <td><input type="number" /></td>
-        <td><input type="number" /></td>
+        <td v-for="player in players" :key="player.id">
+          <input type="number" v-model="points[player.id].litenStege" />
+        </td>
       </tr>
       <tr>
         <th scope="row">Stor stege</th>
-        <td><input type="number" /></td>
-        <td><input type="number" /></td>
-        <td><input type="number" /></td>
-        <td><input type="number" /></td>
-        <td><input type="number" /></td>
-        <td><input type="number" /></td>
+        <td v-for="player in players" :key="player.id">
+          <input type="number" v-model="points[player.id].storStege" />
+        </td>
       </tr>
       <tr>
         <th scope="row">Kåk</th>
-        <td><input type="number" /></td>
-        <td><input type="number" /></td>
-        <td><input type="number" /></td>
-        <td><input type="number" /></td>
-        <td><input type="number" /></td>
-        <td><input type="number" /></td>
+        <td v-for="player in players" :key="player.id">
+          <input type="number" v-model="points[player.id].kak" />
+        </td>
       </tr>
       <tr>
         <th scope="row">Chans</th>
-        <td><input type="number" /></td>
-        <td><input type="number" /></td>
-        <td><input type="number" /></td>
-        <td><input type="number" /></td>
-        <td><input type="number" /></td>
-        <td><input type="number" /></td>
+        <td v-for="player in players" :key="player.id">
+          <input type="number" v-model="points[player.id].chans" />
+        </td>
       </tr>
       <tr>
         <th scope="row">YATZY</th>
-        <td><input type="number" /></td>
-        <td><input type="number" /></td>
-        <td><input type="number" /></td>
-        <td><input type="number" /></td>
-        <td><input type="number" /></td>
-        <td><input type="number" /></td>
+        <td v-for="player in players" :key="player.id">
+          <input type="number" v-model="points[player.id].yatzy" />
+        </td>
       </tr>
 
       <tr class="summa-rad">
         <th scope="row">SUMMA:</th>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
+        <td v-for="player in players" :key="player.id">
+          {{ totalSum(player.id) }}
+        </td>
       </tr>
     </tbody>
   </table>
+  <button type="button" @click="terminateGame">Nytt Spel</button>
 </template>
 
 <style scoped>
@@ -207,5 +208,13 @@ input {
   border: none;
   background: transparent;
   text-align: center;
+}
+
+.player-input {
+  margin-bottom: 1rem;
+  padding: 0.5rem;
+  width: 200px;
+  background-color: #f0f0f0;
+  border: 1px solid #ccc;
 }
 </style>
